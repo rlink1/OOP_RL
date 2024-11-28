5e77db97c00b6a095b1f4e673cadb53a5089cb97
r.link1 <r.link1@share.epsb.ca>
2024-11-28 11:39:47 -0700

    Finished As1

--- src/As2_League/As2_LeagueMain.java
+++ src/As2_League/As2_LeagueMain.java
@@ -1,7 +1,63 @@
 package As2_League;
 
+import Examples.Ex3_Client;
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class As2_LeagueMain {
     public static void run() {
+        ArrayList<As2_Team> allTeams = new ArrayList<>();
+
+        loadFile("data/Teams_ClientData.csv", allTeams);
 
     }
+
+    public static void loadFile(String filename, ArrayList<As2_Team> list ) {
+
+        try {
+            BufferedReader file = new BufferedReader(new FileReader(filename));
+
+            String dataToRead;
+            while( file.ready()){
+                dataToRead = file.readLine();
+
+                String tempArray[] = dataToRead.split(",");
+//the next line is customized for whatever class you are creating.
+//Here we create a new STUDENT so there are 5 variables
+//Unfortunately, you need to Parse any variable that is not a String.  Integers, doubles and booleans are all demonstrated below.
+                list.add( new As2_Team(  tempArray[0],tempArray[1], Integer.parseInt(tempArray[2]), Integer.parseInt(tempArray[3]),Boolean.parseBoolean(tempArray[4]), Double.parseDouble(tempArray[5])   ));
+
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }//end loadFile
+
+    public static void saveFile(String filename, ArrayList <Ex3_Client> tempList ) {
+        try {
+            PrintWriter file = new PrintWriter(new FileWriter(filename));
+
+            for (int i = 0; i < tempList.size(); i++) {
+//the next lines are customized for whatever data you are getting.
+                String toSave ="";
+                toSave = tempList.get(i).getName();  //assumes getter method are used for private variables
+                toSave +="," + tempList.get(i).getAddress();
+                toSave += "," + tempList.get(i).getLawnSize();
+                toSave +="," + tempList.get(i).getHasDog();
+                toSave +="," + tempList.get(i).getOutstandingFees();
+
+//The above 6 lines could be replaced by a call to a carefully made toString() function
+
+                file.println(toSave);
+
+            }
+            file.close();
+        }
+        catch (IOException ex) {
+            System.out.println(ex.toString());
+        }
+
+    }//end saveFile
 }
--- src/As2_League/As2_Team.java
+++ src/As2_League/As2_Team.java
@@ -1,11 +1,26 @@
 package As2_League;
 
+import Examples.Ex3_Client;
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class As2_Team {
 
     private String nickname;
     private String city;
-    private
-    private Boolean hasCoach;
+    private int avgAge;
+    private int numTourneys;
     private Boolean hasWon;
-    private double avgWinning;
+    private double totalMoney;
+
+    public As2_Team(String n, String c, int a, int t, Boolean w, double m){
+        nickname = n;
+        city = c;
+        avgAge = a;
+        numTourneys = t;
+        hasWon = w;
+        totalMoney = m;
+    }
+
 }
